9370f3d5ae14addac38acd85d170032c5d2fe7f7
STEVE <sw523ss@njit.edu>
2019-10-31 00:25:20 -0400

    Feature: Added variable testing to Calculator and testing.

--- src/Calculator.py
+++ src/Calculator.py
@@ -5,6 +5,11 @@ def addition (a, b):
 class Calculator:
     result = 0
 
+    def __init__(self):
+        x = 3 + 3
+        self.result = x;
+        pass
+
     def add(self, a, b):
         self.result = addition(a, b)
         return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -7,6 +7,10 @@ class MyTestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
+        def test_results_property_calculator(self):
+            calculator = Calculator()
+            self.assertEqual(calculator.result, 6)
+
         def test_add_method_calculator(self):
             calculator = Calculator()
             self.assertEqual(calculator.add(3, 3), 6)
